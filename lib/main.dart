8d8a371c99180d0954f6030a42a27dbdfd830bf1
Mohammed Amin <mhmmdmnsdk@gmail.com>
2026-06-22 11:13:40 +0300

    adjusted riverpod version

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import "package:flutter_riverpod/flutter_riverpod.dart";
 
 import 'package:favorite_places/screens/places.dart';
 
@@ -21,7 +22,7 @@ final theme = ThemeData().copyWith(
 );
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:favorite_olaces/main.dart';
+import 'package:favorite_places/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
