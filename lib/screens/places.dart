8050eab93d4a8adfa33f35a9464307ee3166e3af
Mohammed Amin <mhmmdmnsdk@gmail.com>
2026-06-22 12:20:21 +0300

    new places are added succeffully

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,14 +1,17 @@
+import "package:favorite_places/providers/user_places.dart";
 import "package:flutter/material.dart";
 import "package:favorite_places/models/place.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:favorite_places/widgets/places_list.dart";
 
 import "package:favorite_places/screens/add_place.dart";
 
-class PlacesScreen extends StatelessWidget {
+class PlacesScreen extends ConsumerWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
     return Scaffold(
       appBar: AppBar(
         title: Text("Your Places"),
@@ -23,7 +26,7 @@ class PlacesScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: PlacesList(places: []),
+      body: PlacesList(places: userPlaces),
     );
   }
 }
