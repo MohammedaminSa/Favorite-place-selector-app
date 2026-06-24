623b3da0bc2efa89a778732e9594ab1976d4461f
Mohammed Amin <mhmmdmnsdk@gmail.com>
2026-06-24 15:59:55 +0300

    trying to add image to the model

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,14 +1,10 @@
 import "package:uuid/uuid.dart";
+import "dart:io";
 
 class Place {
-  
-
-  Place({
-    required this.title,
-    
-  }) : id = const Uuid().v4();
+  Place({required this.title, required this.image}) : id = const Uuid().v4();
 
   final String id;
   final String title;
- 
+  final File image;
 }
