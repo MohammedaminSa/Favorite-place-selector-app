2661b95bbebbde640bdb5cbab575686aa484fa25
Mohammed Amin <mhmmdmnsdk@gmail.com>
2026-06-24 11:20:05 +0300

    trying to imput image

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:image_picker/image_picker.dart";
+import "dart:io";
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,19 +10,43 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _selectedfImage;
+
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+
+    final pickedImage = await imagePicker.pickImage(source: ImageSource.camera);
+
+    if (pickedImage == null) {
+      return;
+    }
+    setState(() {
+      _selectedfImage = File(pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+      onPressed: _takePicture,
+      icon: const Icon(Icons.camera),
+      label: const Text('Take Picture'),
+    );
+    if (_selectedfImage != null) {
+      content = Image.file(
+        _selectedfImage!,
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      );
+    }
     return Container(
       height: 250,
       width: double.infinity,
 
       decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
       alignment: Alignment.center,
-      child: TextButton.icon(
-        onPressed: () {},
-        icon: const Icon(Icons.camera),
-        label: const Text('Take Picture'),
-      ),
+      child: content,
     );
   }
 }
