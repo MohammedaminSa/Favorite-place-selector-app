f7e02a347e414886206bd35e90feabf0fcd67926
Mohammed Amin <mhmmdmnsdk@gmail.com>
2026-06-26 22:21:50 +0300

    adding location to the model

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,6 +1,13 @@
 import "package:uuid/uuid.dart";
 import "dart:io";
 
+class Location {
+  Location({required this.lat, required this.lng});
+
+  final double lat;
+  final double lng;
+}
+
 class Place {
   Place({required this.title, required this.image}) : id = const Uuid().v4();
 
